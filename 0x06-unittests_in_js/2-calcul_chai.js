99657dffa54baa077cc6d1b763d159f7ff4b66da
Tajudeen <lwltjdn@gmail.com>
2024-08-22 20:39:05 +0100

    basic calculation

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,23 @@
+function calculateNumber(type, a, b) {
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+    let c = 0;
+    switch (type) {
+    case 'SUM':
+	c = aRound + bRound;
+	break;
+    case 'SUBTRACT':
+	c = aRound - bRound;
+	break;
+    case 'DIVIDE':
+      if (bRound === 0) {
+            c = "Error";
+      } else {
+            c = aRound / bRound;
+      }
+      break;
+    }
+    return c;
+}
+
+module.exports = calculateNumber;
